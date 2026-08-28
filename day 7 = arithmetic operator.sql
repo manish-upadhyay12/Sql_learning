fe98625972cb6d3f7ad4da583510e56337d6dde2
Manish Upadhyay <upadhyaymanish0654@gmail.com>
2026-08-28 21:57:12 +0530

    solve few question

--- day 7 = arithmetic operator.sql
+++ day 7 = arithmetic operator.sql
@@ -40,3 +40,41 @@ SELECT first_name,
 	   ROUND(((salary*12)*0.05),2) as anual_tax,
 	   ROUND(salary*12 -((salary*12)*0.5)) as final_salary
 	   from data_practice;
+	   select *from data_practice;
+
+-- Arithmetic operator(*,+,/,%)
+
+-- 1) Salary Increase
+-- Calculate 8% increment on salary and show the new salary
+SELECT first_name,
+       salary,
+       ROUND(salary * 0.08, 2) AS increment_amount,
+       ROUND(salary * 1.08, 2) AS new_salary
+FROM data_practice;
+
+
+-- 2) Yearly Bonus
+-- Calculate annual salary and 15% yearly bonus
+SELECT first_name,
+       salary,
+       ROUND(salary * 12, 2) AS annual_salary,
+       ROUND((salary * 12) * 0.15, 2) AS annual_bonus
+FROM data_practice;
+
+
+-- 3) Salary After Deduction
+-- Deduct 10% tax and 5% insurance from salary
+SELECT first_name,
+       salary,
+       ROUND(salary * 0.10, 2) AS tax,
+       ROUND(salary * 0.05, 2) AS insurance,
+       ROUND(salary - (salary * 0.10) - (salary * 0.05), 2) AS final_salary
+FROM data_practice;
+
+
+-- 4) Remainder Challenge
+-- Find the remainder when salary is divided by 1000
+SELECT first_name,
+       salary,
+       MOD(salary, 1000) AS remainder
+FROM data_practice;
